6a23fbb47149ce4bba0d03e7b0aa61503f86f466
Yug-Dave <yug.dave001@stud.fh-dortmund.de>
2026-05-02 14:50:38 +0200

    feat: enable Spring scheduling for weekly boss battle cron job

--- backend/src/main/java/de/fhdo/HeroSync/HeroSyncApplication.java
+++ backend/src/main/java/de/fhdo/HeroSync/HeroSyncApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Entry point for the HeroSync Spring Boot application.
  */
 @SpringBootApplication
+@EnableScheduling
 public class HeroSyncApplication {
 
   public static void main(String[] args) {
